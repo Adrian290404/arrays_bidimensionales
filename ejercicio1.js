1205e4e764b5af49bd09f21aea9dade0528bec2f
Adrian290404 <adrianmg2004.29@gmail.com>
2025-09-29 10:26:23 +0200

    ejercicio 1

---
+++ ejercicio1.js
@@ -0,0 +1,18 @@
+/* 
+
+1. Crear una matriz 2x2
+
+Crea una matriz de 2 filas y 2 columnas con números enteros e imprímela en consola.
+
+*/
+
+const array = [ [1, 2], [3, 4] ]
+let res = ""
+
+array.forEach(fila => {
+    fila.forEach(valor => {
+        res == "" ? (res = valor) : (res = res + ", " + valor);
+    })
+})
+
+console.log(res)
